5b42d4c9b41c9b862b825f04594ccdc59bd33d02
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2023-12-20 00:55:02 -0500

    load model directly, tokenize history+message, and use cuda with max 200 length

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
-import gradio as gr
-import transformers as trf
+import torch
+import gradio
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-pipeline = trf.pipeline("text-generation", model="microsoft/phi-2", trust_remote_code=True)
+torch.set_default_device("cuda")
+
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
 def chat_handler(message, history):
-    answers = pipeline(message)
+    history_str = '\n'.join(history)
+    inputs = tokenizer.encode_plus(history_str, message, return_tensors="pt", return_attention_mask=False)
+    outputs = outputs = model.generate(**inputs, max_length=200)
+    answers = tokenizer.batch_decode(outputs)
     
     for answer in answers:
-        yield answer["generated_text"]
+        yield answer
 
-interface = gr.ChatInterface(chat_handler)
+interface = gradio.ChatInterface(chat_handler)
 
 if __name__ == "__main__":
     interface.queue().launch(show_api=False)
