5a529a368450ccd65a3ae00b018b3ea356726607
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2023-12-19 23:51:46 -0500

    add chatbot ui

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("phi-2-playground")
+import gradio as gr
+
+def chat_handler(message, history):
+    return "Hello World"
+
+interface = gr.ChatInterface(chat_handler)
+
+if __name__ == "__main__":
+    interface.launch(show_api=False)
