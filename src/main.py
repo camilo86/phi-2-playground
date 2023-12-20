94e48628248d407d27511011885dad7c98d2a607
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2023-12-20 00:36:46 -0500

    hooked up pipeline for phi-2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import gradio as gr
+import transformers as trf
+
+pipeline = trf.pipeline("text-generation", model="microsoft/phi-2", trust_remote_code=True)
 
 def chat_handler(message, history):
-    return "Hello World"
+    answers = pipeline(message)
+    
+    for answer in answers:
+        yield answer["generated_text"]
 
 interface = gr.ChatInterface(chat_handler)
 
 if __name__ == "__main__":
-    interface.launch(show_api=False)
+    interface.queue().launch(show_api=False)
